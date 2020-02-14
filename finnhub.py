bb48bd26e1bff959869be0bd0655fcc553d60d90
Brandon Fan <brandonfan1256@gmail.com>
2020-02-14 18:00:34 -0500

    fix problems

--- finnhub.py
+++ finnhub.py
@@ -1,4 +1,5 @@
 import requests
+from ratelimit import limits, sleep_and_retry
 import time
 
 API_ENDPOINT = 'https://finnhub.io/api/v1/stock/'
@@ -10,6 +11,7 @@ API_SENTIMENT_ENDPOINT = 'https://finnhub.io/api/v1/news-sentiment'
 
 class FinnHub(): 
     def __init__(self, symbol, token): 
+        self.failed = []
         self._stock_data = {
             'profile': {},
             'ceo_compensation': {},
@@ -27,56 +29,41 @@ class FinnHub():
 
     def _check_request(self, r):
         if r.status_code == 429:
-            print('Waiting for 1 Minute')
-            time.sleep(60)
+            time.sleep(50)
             return True
         else:
             return False
+    
     def _send_request(self, endpoint):
-        url_string = API_ENDPOINT + endpoint + '?symbol={0}&token={1}'.format(self.symbol, self.token)
-        r = requests.get(url_string)
+        def generate_url():
+            if endpoint == 'support-resistance' or endpoint == 'technical-indicator':
+                url_string = API_TECHNICAL_ENDPOINT + endpoint + '?symbol={0}&resolution=D&token={1}'.format(self.symbol, self.token)
+            elif endpoint == 'quote':
+                url_string = API_QUOTE_ENDPOINT + '?symbol={0}&token={1}'.format(self.symbol, self.token)
+            elif endpoint == 'news':
+                url_string = API_NEWS_ENDPOINT + '{0}?token={1}'.format(self.symbol, self.token)
+            elif endpoint == 'developments':
+                url_string = API_DEVELOPMENTS_ENDPOINT  + '?symbol={0}&token={1}'.format(self.symbol, self.token)
+            elif endpoint == 'news_sentiment':
+                url_string = API_SENTIMENT_ENDPOINT + '?symbol={0}&token={1}'.format(self.symbol, self.token)
+            else:
+                url_string = API_ENDPOINT + endpoint + '?symbol={0}&token={1}'.format(self.symbol, self.token)
+            return url_string
+        url_string = generate_url()
+        try:
+            r = requests.get(url_string, timeout=(2, 10))
+        except Exception as e:
+            print(e)
+            self.failed.append(url_string)
+            return {}
         if self._check_request(r):
-            r = requests.get(url_string)
-            return r.json()
-        return r.json()
-
-    def _send_technical_request(self, endpoint):
-        url_string = API_TECHNICAL_ENDPOINT + endpoint + '?symbol={0}&resolution=D&token={1}'.format(self.symbol, self.token)
-        r = requests.get(url_string)
-        if self._check_request(r):
-            r = requests.get(url_string)
-            return r.json()
-        return r.json()
-
-    def _send_quote_request(self):
-        url_string = API_QUOTE_ENDPOINT + '?symbol={0}&token={1}'.format(self.symbol, self.token)
-        r = requests.get(url_string)
-        if self._check_request(r):
-            r = requests.get(url_string)
-            return r.json()
-        return r.json()
-
-    def _send_news_request(self, endpoint):
-        url_string = API_NEWS_ENDPOINT + '{0}?token={1}'.format(self.symbol, self.token)
-        r = requests.get(url_string)
-        if self._check_request(r):
-            r = requests.get(url_string)
-            return r.json()
-        return r.json()
-
-    def _send_developments_request(self):
-        url_string = API_DEVELOPMENTS_ENDPOINT  + '?symbol={0}&token={1}'.format(self.symbol, self.token)
-        r = requests.get(url_string)
-        if self._check_request(r):
-            r = requests.get(url_string)
-            return r.json()
-        return r.json()
-
-    def _send_news_sentiment_request(self):
-        url_string = API_SENTIMENT_ENDPOINT + '?symbol={0}&token={1}'.format(self.symbol, self.token)
-        r = requests.get(url_string)
-        if self._check_request(r):
-            r = requests.get(url_string)
+            try:
+                url_string = generate_url()
+                r = requests.get(url_string, timeout=(2, 10))
+            except Exception as e:
+                print(e)
+                self.failed.append(url_string)
+                return {}
             return r.json()
         return r.json()
 
@@ -84,12 +71,12 @@ class FinnHub():
         self._stock_data['profile'] = self._send_request('profile')
         self._stock_data['ceo_compensation'] = self._send_request('ceo-compensation')
         self._stock_data['recommendation'] = self._send_request('recommendation')
-        self._stock_data['quote'] = self._send_quote_request()
-        self._stock_data['levels'] = self._send_technical_request('support-resistance')
-        self._stock_data['indicators'] = self._send_technical_request('technical-indicator')
-        self._stock_data['news'] = self._send_news_request('general')
-        self._stock_data['developments'] = self._send_developments_request()
-        self._stock_data['news_sentiment'] = self._send_news_sentiment_request()
+        self._stock_data['quote'] = self._send_request('quote')
+        self._stock_data['levels'] = self._send_request('support-resistance')
+        self._stock_data['indicators'] = self._send_request('technical-indicator')
+        self._stock_data['news'] = self._send_request('news')
+        self._stock_data['developments'] = self._send_request('developments')
+        self._stock_data['news_sentiment'] = self._send_request('news_sentiment')
 
     def stock_data(self):
         return self._stock_data
--- main.py
+++ main.py
@@ -12,19 +12,21 @@ def get_token(filepath):
     with open(os.path.abspath(filepath)) as f:
         data = f.readlines()
         f.close()
-    return data[0]
+    return data
 
 def main():
     now = datetime.now()
     string = now.strftime('%m-%d-%Y')
     token = get_token('./token.txt')
+    token = [string.strip() for string in token]
     package = Package('https://datahub.io/core/s-and-p-500-companies/datapackage.json')
     sp500companies = package.get_resource('constituents').read()
     sp500_stock_data = []
-
-    for company in tqdm(sp500companies): 
+    failed_urls = []
+    for index, company in enumerate(tqdm(sp500companies)): 
         symbol = company[0]
-        finn_symbol = FinnHub(symbol, token)
+        token_index = index % len(token)
+        finn_symbol = FinnHub(symbol, token[token_index])
         sp500_stock_data.append(finn_symbol.stock_data())
     
     if not os.path.exists('./stock_data'):
@@ -38,8 +40,9 @@ def main():
 
 
 if __name__ == "__main__":
-    schedule.every().day.at('01:00').do(main)
+    # schedule.every().day.at('01:00').do(main)
 
-    while True:
-        schedule.run_pending()
-        time.sleep(60)
+    # while True:
+    #     schedule.run_pending()
+    #     time.sleep(60)
+    main()
